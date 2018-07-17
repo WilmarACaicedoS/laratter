255d36e14d32985c09d6270a501ea6129be1c6c0
Wilmar Caicedo <wilmarcaicedo@hotmail.com>
2018-07-17 15:36:41 -0500

    guardar notificaciones y exponerlas en tiempo real

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -108,4 +108,9 @@ class UsersController extends Controller
     {
         return User::where('username', $username)->firstOrFail();
     }
+
+    public function notifications(Request $request)
+    {
+        return $request->user()->notifications;        
+    }
 }
